ea722132e32b832398b32055ad9168124bd38be5
Kwan Sohn <ksohn18@gmail.com>
2017-01-18 20:05:20 -0500

    Commit #11
    Removed default value for event.time

--- db/migrate/20170118070145_add_time_to_events.rb
+++ db/migrate/20170118070145_add_time_to_events.rb
@@ -1,5 +1,5 @@
 class AddTimeToEvents < ActiveRecord::Migration[5.0]
   def change
-      add_column :events, :time, :datetime, :null => false, :default => 0
+      add_column :events, :time, :datetime, :null => false
   end
 end
